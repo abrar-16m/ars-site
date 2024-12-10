395dfcc91ef444a91d02f92a1b863f88580e7907
Malik Araf <arafmalik04@gmail.com>
2024-12-10 18:06:46 +0600

    same as last one

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import styles from "./page.module.css";
 import Hero from "@/components/Home/Hero/Hero";
 import HowItWorks from "@/components/Home/HowItWorks/HowItWorks";
 import News from "@/components/Home/News/News";
-import Contact from "@/components/Home/Contact/Contact";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/sections/footer/Footer";
 
